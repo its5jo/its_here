7e908f812c38f7546ecbb65e3c46019e7783f01d
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-07-06 13:11:55 +0900

    init

---
+++ src/main/java/com/spring/its_here/ItsHereApplication.java
@@ -0,0 +1,13 @@
+package com.spring.its_here;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsHereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/its_here/ItsHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.its_here;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
