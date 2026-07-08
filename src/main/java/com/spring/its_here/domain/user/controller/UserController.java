096e5e5154aefa0b2099ba9cbbf20891623583ea
rvbear <rvbear2000@gmail.com>
2026-07-08 11:01:19 +0900

    feat: Swagger Docs 작성

--- src/main/java/com/spring/its_here/domain/user/controller/UserController.java
+++ src/main/java/com/spring/its_here/domain/user/controller/UserController.java
@@ -7,8 +7,11 @@ import com.spring.its_here.domain.user.dto.response.UserResponseDto;
 import com.spring.its_here.domain.user.dto.response.UserSelfGetResponseDto;
 import com.spring.its_here.domain.user.dto.response.UserTokenResponseDto;
 import com.spring.its_here.domain.user.service.UserService;
-import com.spring.its_here.global.ApiResponse;
+import com.spring.its_here.global.response.ApiResponse;
 import com.spring.its_here.global.security.CookieUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "사용자", description = "사용자 관련 API 입니다.")
 @RequestMapping("/api/users")
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +29,11 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(
+            summary = "회원가입",
+            description = "ID와 PW, nickname과 role을 이용하여 회원가입합니다."
+    )
+    @SecurityRequirements()
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<UserResponseDto>> signup(
             @Valid @RequestBody UserCreateRequestDto userCreateRequestDto
@@ -34,11 +43,15 @@ public class UserController {
                 .status(HttpStatus.CREATED)
                 .body(ApiResponse.success(
                         "회원가입 성공",
-                        "SUCCESS",
                         userService.signup(userCreateRequestDto)
                 ));
     }
 
+    @Operation(
+            summary = "로그인",
+            description = "ID와 PW를 이용하여 Access Token, Refresh Token을 발급합니다."
+    )
+    @SecurityRequirements()
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<UserTokenResponseDto>> login(
             @Valid @RequestBody UserLoginRequestDto userLoginRequestDto,
@@ -59,13 +72,17 @@ public class UserController {
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success(
                         "로그인 성공",
-                        "SUCCESS",
                         new UserTokenResponseDto(
                                 tokenPairDto.accessToken()
                         )
                 ));
     }
 
+    @Operation(
+            summary = "토큰 재발급",
+            description = "Refresh Token을 통해 Access Token과 Refresh Token을 재발급합니다."
+    )
+    @SecurityRequirements()
     @PostMapping("/reissue")
     public ResponseEntity<ApiResponse<UserTokenResponseDto>> reissue(
             HttpServletRequest request,
@@ -92,7 +109,6 @@ public class UserController {
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success(
                         "재발급 성공",
-                        "SUCCESS",
                         new UserTokenResponseDto(
                                 tokenPairDto.accessToken()
                         )
@@ -100,13 +116,16 @@ public class UserController {
         );
     }
 
+    @Operation(
+            summary = "내 정보 조회",
+            description = "권한이 있는 사용자가 자신의 정보를 조회합니다."
+    )
     @GetMapping("/me")
     public ResponseEntity<ApiResponse<UserSelfGetResponseDto>> getCurrentUser() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success(
                         "내 정보 조회 성공",
-                        "SUCCESS",
                         userService.getSelf()
                 ));
     }
