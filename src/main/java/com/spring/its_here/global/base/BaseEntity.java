b3d732a746aa54fc81ac1b6537d515f7daef89ad
taeeonkim <myhom43@gmail.com>
2026-07-08 11:34:55 +0900

    feature: 초기 BaseEntity 클래스 추가
    공통 필드 생성일, 생성자 클래스 BaseEntity 초기 설계 추가

---
+++ src/main/java/com/spring/its_here/global/base/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.spring.its_here.global.base;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+}
