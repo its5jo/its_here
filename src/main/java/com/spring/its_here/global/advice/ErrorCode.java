694c99ce6533bb62e6bf624616300d4c2bc995b0
skdysla <skdysla@gmail.com>
2026-07-09 10:05:55 +0900

    feat: 에러코드에 DUPLICATE_CATEGORY_NAME 추가

--- src/main/java/com/spring/its_here/global/advice/ErrorCode.java
+++ src/main/java/com/spring/its_here/global/advice/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
     AUTH_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 올바르지 않습니다.", "U-001"),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "입력조건을 불충족하였습니다.", "U-002"),
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "동일 아이디가 존재합니다.", "U-003"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.", "U-004");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.", "U-004"),
+    DUPLICATE_CATEGORY_NAME(HttpStatus.CONFLICT, "동일한 카테고리가 존재합니다.", "C-001");
 
     private final String message;
     private final HttpStatus status;
