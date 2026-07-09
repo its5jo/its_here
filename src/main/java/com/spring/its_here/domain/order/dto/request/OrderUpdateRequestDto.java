3ccb04478989863d979eaf4a7a8f3d832e27a06f
coco <meunse1999@gmail.com>
2026-07-09 14:25:37 +0900

    feat: OrderUpdateRequestDto 삭제

--- src/main/java/com/spring/its_here/domain/order/dto/request/OrderUpdateRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.spring.its_here.domain.order.dto.request;
-
-import com.spring.its_here.domain.order.enums.OrderStatus;
-
-public record OrderUpdateRequestDto(
-        OrderStatus status
-) {
-}
