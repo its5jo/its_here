aa91cfbee4422b212bba05dcd4e3919b44c4581d
skdysla <skdysla@gmail.com>
2026-07-10 21:19:09 +0900

    refactor: Category 생성 로직 변경 및 hasDeleted 삭제

--- src/main/java/com/spring/its_here/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/spring/its_here/domain/category/repository/CategoryRepository.java
@@ -1,15 +1,15 @@
 package com.spring.its_here.domain.category.repository;
 
 import com.spring.its_here.domain.category.entity.Category;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
-    boolean existsByNameAndHasDeletedFalse(String name);
+    boolean existsByNameAndDeletedAtIsNull(String name);
+
+    Optional<Category> findByIdAndDeletedAtIsNull(UUID categoryId);
 }
