586e3866178811bf57c924bb0a0366ac62315a14
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-07-10 12:48:47 +0900

    merge: develop 변경사항 반영

--- src/main/java/com/spring/its_here/global/advice/ErrorCode.java
+++ src/main/java/com/spring/its_here/global/advice/ErrorCode.java
@@ -12,10 +12,11 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.", "U-004"),
     DUPLICATE_CATEGORY_NAME(HttpStatus.CONFLICT, "동일한 카테고리가 존재합니다.", "C-001"),
     INVALID_RATING(HttpStatus.BAD_REQUEST, "유효하지 않은 평점", "U-001"),
-    CONTENT_TOO_LONG(HttpStatus.BAD_REQUEST, "리뷰내용 255자 이하", "R-002"),
+    REVIEW_CONTENT_VALID(HttpStatus.BAD_REQUEST, "리뷰내용 255자 이하입니다", "R-002"),
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 주문입니다.", "O-001"),
-    REVIEW_FORBIDDEN(HttpStatus.NOT_FOUND, "리뷰작성 권한이 없습니다.", "R-001")
-    ;
+    REVIEW_FORBIDDEN(HttpStatus.NOT_FOUND, "리뷰작성 권한이 없습니다.", "R-001"),
+    REVIEW_ORDER_NOT_COMPLETED(HttpStatus.NOT_FOUND, "완료된 주문에만 리뷰를 작성할 수 있습니다.", "R-003"),
+    REVIEW_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 리뷰입니다.", "R-004");
 
     private final String message;
     private final HttpStatus status;
