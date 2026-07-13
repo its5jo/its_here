edc114a6fc5d6a06fd814f502f6b1cff05a459bf
coco <meunse1999@gmail.com>
2026-07-13 18:15:07 +0900

    feat: StoreRepository 조회 메서드 추가

--- src/main/java/com/spring/its_here/domain/store/repository/StoreRepository.java
+++ src/main/java/com/spring/its_here/domain/store/repository/StoreRepository.java
@@ -2,11 +2,15 @@ package com.spring.its_here.domain.store.repository;
 
 import com.spring.its_here.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StoreRepository extends JpaRepository<Store, UUID>, StoreRepositoryCustom {
 
     boolean existsByNameAndDeletedAtIsNull(String name);
     boolean existsByUserIdAndDeletedAtIsNull(Long userId);
+
+    Optional<Store> findByUserIdAndDeletedAtIsNull(Long userId);
 
 }
